20644e6d538a5633e2774a87ca74220462b4c722
kedinaimoke <jkoimoks01@gmail.com>
2022-06-06 14:31:00 +0100

    Included a try and except method in views for a non-existing word.

--- dictdjango/dictsite/dictionary/views.py
+++ dictdjango/dictsite/dictionary/views.py
@@ -10,14 +10,17 @@ import requests
 def index(request):
     if request.method == 'POST':
         word = request.POST.get('word')
-        url = 'https://www.dictionary.com/browse/' + word
-        r = requests.get(url)
-        data = r.content
-        soup = BeautifulSoup(data, 'html.parser')
-        span = soup.find_all('span', {'class': 'one-click-content'})
-        context = {
-            'text': span[0].text,
-            'word': word
-        }
-        return render(request, 'dictionary/index.html', context)
+        try:
+            url = 'https://www.dictionary.com/browse/' + word
+            r = requests.get(url)
+            data = r.content
+            soup = BeautifulSoup(data, 'html.parser')
+            span = soup.find_all('span', {'class': 'one-click-content'})
+            context = {
+                'text': span[0].text,
+                'word': word
+            }
+            return render(request, 'dictionary/index.html', context)
+        except:
+            return HttpResponse(f"<h1>'{word}' does not exist.</h1>")
     return render(request, 'dictionary/index.html')
